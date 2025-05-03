03eaa0cbc4363dd6b3ba2e1884436fca6656496d
HAlGhanim <imzaji98@gmail.com>
2025-05-03 19:28:42 +0300

    Initial push did nothing yet

---
+++ src/app/services/transactions/transactions.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import { BaseService } from '../base/base.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionsService extends BaseService {}
