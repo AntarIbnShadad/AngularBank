79ccd5200386be5dfa377ef5cca4cca44f9d767b
Abdulrahman <AYAlkandari@pifss.gov.kw>
2025-05-05 11:44:48 +0300

    --Profile page created. --profile routing added to navbar. --user profile fethed and displayed

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../../interfaces/interfaces';
+import { UsersService } from '../../services/users/users.service';
 
 @Component({
   selector: 'app-profile',
@@ -8,5 +10,16 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ProfileComponent {
+user: User = {
+  _id: '',
+  username: undefined,
+  image: undefined,
+  balance: 0,
+}
 
+constructor(private _user: UsersService){}
+
+ngOnInit(){
+  this._user.getProfile().subscribe(response => {this.user = response})
+}
 }
